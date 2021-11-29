3fd2f727e2dbb223a3187587ac5a0f99125d7920
unknown <khanghuy06042000@gmail.com>
2021-11-29 17:15:55 +0700

    division

--- Division/checkinf.v
+++ Division/checkinf.v
@@ -1,7 +1,9 @@
-module checkinf(a,f);
+module checkinf(a,b,f);
 input[7:0] a;
-output f;
+input[22:0] b;
+output[1:0] f;
 wire[7:0] n;
+wire[22:0] m;
 xnor(n[0],a[0],1'b1);
 xnor(n[1],a[1],1'b1);
 xnor(n[2],a[2],1'b1);
@@ -10,5 +12,30 @@ xnor(n[4],a[4],1'b1);
 xnor(n[5],a[5],1'b1);
 xnor(n[6],a[6],1'b1);
 xnor(n[7],a[7],1'b1);
-and(f,n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7]);
+and(f[0],n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7]); /// Check exp co bang 0 ko ?
+
+xnor(m[0],b[0],1'b0);
+xnor(m[1],b[1],1'b0);
+xnor(m[2],b[2],1'b0);
+xnor(m[3],b[3],1'b0);
+xnor(m[4],b[4],1'b0);
+xnor(m[5],b[5],1'b0);
+xnor(m[6],b[6],1'b0);
+xnor(m[7],b[7],1'b0);
+xnor(m[8],b[8],1'b0);
+xnor(m[9],b[9],1'b0);
+xnor(m[10],b[10],1'b0);
+xnor(m[11],b[11],1'b0);
+xnor(m[12],b[12],1'b0);
+xnor(m[13],b[13],1'b0);
+xnor(m[14],b[14],1'b0);
+xnor(m[15],b[15],1'b0);
+xnor(m[16],b[16],1'b0);
+xnor(m[17],b[17],1'b0);
+xnor(m[18],b[18],1'b0);
+xnor(m[19],b[19],1'b0);
+xnor(m[20],b[20],1'b0);
+xnor(m[21],b[21],1'b0);
+xnor(m[22],b[22],1'b0);    ///Neu f[1]=1 --> inf, f[1]=0 --> NAN
+and(f[1],m[0],m[1],m[2],m[3],m[4],m[5],m[6],m[7],m[8],m[9],m[10],m[11],m[12],m[13],m[14],m[15],m[16],m[17],m[18],m[19],m[20],m[21],m[22]);
 endmodule	
--- Division/sub_exp.v
+++ Division/sub_exp.v
@@ -1,4 +1,4 @@
-module fadder(x, y, cin, s, cout);   //b? full adder
+module fadder(x, y, cin, s, cout);
 input x, y, cin;
 output s, cout;
 wire m1,m2,m3;
@@ -9,7 +9,7 @@ and(m3,x,y);
 or(cout,m3,m2);
 endmodule
 
-module add9b(a2, b2, ci2, s2, cout2);  ///b? c?ng tr? 8 bit
+module add9b(a2, b2, ci2, s2, cout2); 
 input[8:0] a2,b2;
 input ci2; 
 output[8:0] s2;
@@ -31,15 +31,48 @@ fadder fadder3(a2[2], t[2], w[1], s2[2],w[2]);
 fadder fadder4(a2[3], t[3], w[2], s2[3],w[3]);
 fadder fadder5(a2[4], t[4], w[3], s2[4],w[4]);
 fadder fadder6(a2[5], t[5], w[4], s2[5],w[5]);
-fadder fadder7(a2[6], t[6], w[5], s2[6],w[6]);   
+fadder fadder7(a2[6], t[6], w[5], s2[6],w[6]);
 fadder fadder8(a2[7], t[7], w[6], s2[7],w[7]);
 fadder fadder9(a2[8], t[8], w[7], s2[8],cout2);
 endmodule
 
-module normalizeExpo(num,expo,newexp);   //b? chu?n hóa 8 bit m? s? (Exponent)
-input[4:0] num;
-input[8:0] expo;
-output[8:0] newexp;
-wire co;
-add9b chuanhoamu(expo, {4'b0,num[4:0]},1'b1,newexp,co);
+module bu2(x2, y2);
+input[8:0] x2;
+output[8:0] y2;
+wire[8:0] n0;
+wire Cout2;
+	not(n0[0], x2[0]);
+	not(n0[1], x2[1]);
+	not(n0[2], x2[2]);
+	not(n0[3], x2[3]);
+	not(n0[4], x2[4]);
+	not(n0[5], x2[5]);
+	not(n0[6], x2[6]);
+	not(n0[7], x2[7]);
+	not(n0[7], x2[8]);
+	add9b add1(n0[8:0],9'd1,1'b0,y2[8:0],Cout2);         
+endmodule 
+
+module mux2_9b(a3, b3, s3, f3);
+input[8:0] a3, b3;
+input s3;
+output reg[8:0] f3;
+always @(a3 or b3 or s3) 
+begin
+	if(s3)
+		f3 = a3; 
+	else 
+		f3 = b3;
+end
 endmodule
+
+module sub_exp(x,y,z);
+input[8:0] x,y;
+output[8:0] z;
+wire c;
+wire[8:0] z1, z2;
+add9b u1(x, y, 1'b1, z1, c);
+bu2 u2(z1, z2);
+mux2_9b u3(z1, z2, c, z);
+endmodule
+
